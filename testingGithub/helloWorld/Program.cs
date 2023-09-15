f3318691f7f1d7e0d700b4b7fc787840179edafd
chandlerekholm <111912324+chandlerekholm@users.noreply.github.com>
2023-09-15 16:49:17 -0500

    hey just testing stuff

---
+++ testingGithub/helloWorld/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace goodJob
+{
+    class youDidIt
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hey nice you have our stuff on your machine now :)");
+        }
+    }
+}
