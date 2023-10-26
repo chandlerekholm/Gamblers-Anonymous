78d4b528b9f5efe2dd019f4ceedea3f38b1ec350
bscallies <99484744+bscallies@users.noreply.github.com>
2023-10-26 17:51:08 -0500

    Create GameHistory.cs

---
+++ GameHistory.cs
@@ -0,0 +1,14 @@
+class GameHistory
+{
+    List<GameRecord> history;
+
+    public void AddRecord(GameRecord record)
+    {
+        // Add a new game record to history
+    }
+
+    public List<GameRecord> GetHistory()
+    {
+        // Return user's game history
+    }
+}
