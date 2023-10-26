2acad73f22c6297abc6a9ba5d5109a7cca2daca2
bscallies <99484744+bscallies@users.noreply.github.com>
2023-10-26 17:50:49 -0500

    Create Lobby.cs

---
+++ Lobby.cs
@@ -0,0 +1,14 @@
+class Lobby
+{
+    List<GameRoom> availableRooms;
+
+    public void ShowRooms()
+    {
+        // Display available rooms
+    }
+
+    public void JoinRoom(GameRoom room)
+    {
+        // Join selected room
+    }
+}
