9243efecc78b809ef5f4f4c0555e913f8ef0639e
ArcherZen <38820608+ArcherZen@users.noreply.github.com>
2023-11-04 10:56:02 -0600

    Add files via upload

---
+++ CSC470_TH/CSC470_TH/Card.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC470_TH
+{
+    public class Card
+    {
+
+        public int ID { get; }
+        public string Suit { get; }
+        public string Value { get; }
+
+        public Card(int id, string suit, string value)
+        {
+            ID = id;
+            Suit = suit;
+            Value = value;
+        }
+
+    }
+}
---
+++ CSC470_TH/CSC470_TH/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC470_TH // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Player player1 = new Player("John", 500, 5);
+
+            player1.Check();
+
+            Player player2 = new Player("Sam", 450, 2);
+
+            player2.Raise(25);
+
+            Dealer dealer = new Dealer("Vegas Dealer");
+
+            dealer.Shuffle();
+
+            foreach (Card c in dealer.Deck)
+            {
+                Console.WriteLine(c.Value + " of " + c.Suit + " card id = " + c.ID);
+            }
+
+
+            
+
+        }
+    }
+}
+
+
