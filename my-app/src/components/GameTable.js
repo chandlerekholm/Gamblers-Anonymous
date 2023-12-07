c9412b64cb7095997d18744a309e5bada9eb7079
bscallies <99484744+bscallies@users.noreply.github.com>
2023-12-06 20:20:45 -0600

    Greatly expanded on rewrote much of the gametable code. Add placeholder for API calls and added error checking for API call failures. Saving progress while I try to debug some issues.

--- my-app/src/components/GameTable.js
+++ my-app/src/components/GameTable.js
@@ -1,23 +1,69 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Card from './Card';
+import Player from './Player';
+import Pot from './Pot';
+import './GameTable.css';
 
-const GameTable = () => {
+const GameTable = ({ playerName }) => {
+    const navigate = useNavigate();
+    const [players, setPlayers] = useState([
+        { name: "<empty>", chips: 0 },
+        { name: "<empty>", chips: 0 },
+        { name: playerName, chips: 1500 }, // Your player
+        { name: "<empty>", chips: 0 },
+    ]);
+    const [apiError, setApiError] = useState('');
+    const communityCards = [{ rank: '?', suit: '' }, { rank: '?', suit: '' }, { rank: '?', suit: '' }];
+
     useEffect(() => {
-        // Call backend to start a game session
-        console.log('Starting a new game session...');
-        // backend API call would go here
+        const checkPlayers = async () => {
+            try {
+                // Attempt to fetch data from the API
+                const response = await fetch('/api/check-players');
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const data = await response.json();
+                setPlayers(data.players);
+                setApiError(''); // Clear any existing error
+            } catch (error) {
+                // Catch any errors and set an error message
+                setApiError('Failed to load players. Retrying...');
+                console.error('There was a problem with the fetch operation:', error);
+            }
+        };
+
+        // Call the function immediately, and then every 30 seconds
+        checkPlayers();
+        const intervalId = setInterval(checkPlayers, 30000);
+
+        // Clear the interval on component unmount
+        return () => clearInterval(intervalId);
     }, []);
 
+    const handleExit = () => navigate('/lobby'); // Adjust the route as needed
+
     return (
-        <div>
-            <h1>Texas Holdem Game</h1>
-            <p>Game session is starting...</p>
-            {/* placeholder for game visuals */}
-            <div>
-                {/* display cards, chips, etc. */}
-                <p>Game table visuals go here...</p>
+        <div className="table-container">
+            {apiError && <div className="api-error">{apiError}</div>}
+            <div className="game-table">
+                <div className="community-cards">
+                    {/* Render face-down community cards */}
+                    <div className="card facedown"></div>
+                    <div className="card facedown"></div>
+                    <div className="card facedown"></div>
+                </div>
+                <div className="pot">Pot: $300</div>
             </div>
+            <div className="players">
+                {/* Render player components */}
+                {/* ... */}
+            </div>
+            <button className="exit-button">Exit Game</button>
         </div>
     );
 };
 
 export default GameTable;
+
