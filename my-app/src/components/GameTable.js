e9b1591eb737179b1fb2b81f56e94a79eea6754a
bscallies <99484744+bscallies@users.noreply.github.com>
2023-12-09 16:20:29 -0600

    Added player boxes to GameTable.js Fixed conflicting styling Cleaned up table styling Improved GameTable.js logic

--- my-app/src/components/GameTable.js
+++ my-app/src/components/GameTable.js
@@ -8,11 +8,12 @@ import './GameTable.css';
 const GameTable = ({ playerName }) => {
     const navigate = useNavigate();
     const [players, setPlayers] = useState([
+        { name: playerName, chips: 1500, isTurn: false },
         { name: "<empty>", chips: 0 },
-        { name: "<empty>", chips: 0 },
-        { name: playerName, chips: 1500 }, // Your player
+        { name: "<empty>", chips: 0 }, // Your player
         { name: "<empty>", chips: 0 },
     ]);
+    const [currentTurn, setCurrentTurn] = useState(null);
     const [apiError, setApiError] = useState('');
     const communityCards = [{ rank: '?', suit: '' }, { rank: '?', suit: '' }, { rank: '?', suit: '' }];
 
@@ -29,9 +30,12 @@ const GameTable = ({ playerName }) => {
                 setApiError(''); // Clear any existing error
             } catch (error) {
                 // Catch any errors and set an error message
-                setApiError('Failed to load players. Retrying...');
+                setApiError('API error. Retrying...');
                 console.error('There was a problem with the fetch operation:', error);
             }
+        };
+        const handlePlayerAction = (action) => {
+            // API call sending the player's action to the backend
         };
 
         // Call the function immediately, and then every 30 seconds
@@ -42,11 +46,19 @@ const GameTable = ({ playerName }) => {
         return () => clearInterval(intervalId);
     }, []);
 
+    const handlePlayerAction = (action) => {
+        // API call for player's action
+    };
     const handleExit = () => navigate('/lobby'); // Adjust the route as needed
 
     return (
         <div className="table-container">
-            {apiError && <div className="api-error">{apiError}</div>}
+            {apiError && (
+                <>
+                    <div className="api-error">{apiError}</div>
+                    <p className="waiting-text">Waiting for other players to join...</p>
+                </>
+            )}
             <div className="game-table">
                 <div className="community-cards">
                     {/* Render face-down community cards */}
@@ -54,13 +66,24 @@ const GameTable = ({ playerName }) => {
                     <div className="card facedown"></div>
                     <div className="card facedown"></div>
                 </div>
-                <div className="pot">Pot: $300</div>
+                <Pot amount={300} />
             </div>
-            <div className="players">
-                {/* Render player components */}
-                {/* ... */}
+            <div className="player-container">
+                {players.map((player, index) => (
+                    <div className="player-box" key={index}>
+                        <div>{player.name}</div>
+                        <div className="chips">Chips: {player.chips}</div>
+                        <div className="player-actions">
+                            {/* Render buttons here, make sure to conditionally render based on player's turn */}
+                            <button className="button">Fold</button>
+                            <button className="button">Call</button>
+                            <button className="button">Raise</button>
+                        </div>
+                    </div>
+                ))}
             </div>
-            <button className="exit-button">Exit Game</button>
+
+            <button className="exit-button" onClick={handleExit}>Exit Game</button>
         </div>
     );
 };
