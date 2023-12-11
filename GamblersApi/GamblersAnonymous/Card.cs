08278a357840dda88b9f7e6dd73d6ea951b892fd
Dakota List <daklist@gmail.com>
2023-12-10 21:59:58 -0700

    beginning framework for rest api

---
+++ GamblersApi/GamblersAnonymous/Card.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamblersAnonymous
+{
+    public class Card
+    {
+        public int ID { get; }
+        public string Suit { get; }
+        public int Value { get; }
+
+        // simple data structure for a card
+        public Card(int id, string suit, int value)
+        {
+            ID = id;
+            Suit = suit;
+            Value = value;
+        }
+
+    }
+}
